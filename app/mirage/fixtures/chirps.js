4754f4093e6eba584ea238f237501d0b68af9d48
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-27 16:14:35 +0300

    Fill followers/followings pages

--- app/mirage/fixtures/chirps.js
+++ app/mirage/fixtures/chirps.js
@@ -8,4 +8,9 @@ export default [{
   text: "Hello again!",
   user: 1,
   createdAt: new Date('2015-06-08T09:30:27')
+}, {
+  id: 3,
+  text: 'Hello one more time!',
+  user: 1,
+  createdAt: new Date('2015-06-08T09:30:28')
 }];
--- app/mirage/fixtures/users.js
+++ app/mirage/fixtures/users.js
@@ -1,10 +1,9 @@
 export default [{
   id: 1,
   username: 't4t5',
-  numberOfChirps: 2,
-  numberOfFollowing: 5,
-  numberOfFollowers: 5, // The new data:
   aboutMe: 'I like making stuff.',
   joinedAt: new Date('2015-06-08T09:30:26'),
-  chirps: [1, 2]
+  followees: [1],
+  followers: [1],
+  chirps: [1, 2, 3] // <-- Add the "3"
 }];
