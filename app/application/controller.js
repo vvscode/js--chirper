7b7c4f3c7f1e69f576decd3d5b7dd5f90ef2f2b4
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-29 22:48:41 +0300

    Add modal

--- app/application/controller.js
+++ app/application/controller.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 const { Controller } = Ember;
 
 export default Controller.extend({
-  appName: "Chirper"
+  appName: "Chirper",
+  showingComposeModal: false,
+
+  actions: {
+    toggleComposeModal: function() {
+      // Toggle boolean value
+      this.set('showingComposeModal', !this.get('showingComposeModal'));
+    }
+  }
 });
