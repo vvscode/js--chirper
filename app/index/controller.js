009fa46f92e2c5128f3a15c27bccc1d2e2a4be32
Vasil Vanchuk <v.vanchuk@tut.by>
2016-01-31 16:51:04 +0300

    Add sign-up functional

--- app/index/controller.js
+++ app/index/controller.js
@@ -1,20 +1,39 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Controller.extend({
+  session: Ember.inject.service('session'),
+
   valuesChanged: Ember.observer('username', 'password', function() {
     this.set('errorMessage', false);
   }),
   actions: {
-    login() {
+    signup: function() {
+      var userData = {
+        username: this.get('username'),
+        password: this.get('password')
+      };
+
+      Ember.$.ajax({
+          type: 'POST',
+          url: config.apiURL + '/signup',
+          dataType: 'json',
+          data: userData
+        })
+        .done(() => {
+          console.log("Created!");
+          this.send('login');
+        })
+        .fail(() => {
+          this.set('errorMessage', "Couldn't sign up!");
+        });
+    },
+
+    login: function() {
       var username = this.get('username');
       var password = this.get('password');
 
-      if (username === 'letme' && password === 'in') {
-        this.transitionToRoute('home');
-      } else {
-        // We replace our SweetAlert-function with a simple variable containing the error message
-        this.set('errorMessage', 'Wrong username or password!');
-      }
+      console.log(`Trying to log in with ${username} and ${password}`);
     }
   }
 });
