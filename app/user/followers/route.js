8e9758f2d03decd3700bcc1dfe018783bc79ff7c
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-02 21:03:51 +0300

    Load followers/followings

--- app/user/followers/route.js
+++ app/user/followers/route.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
+  model: function (params, transition) {
+    var username = transition.params.user.username;
+
     return Ember.RSVP.hash({
-      users: this.store.findAll('user')
+      users: this.store.query('user', {
+        followee: username
+      })
     });
   }
 });
