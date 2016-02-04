f5ff1ee2fb782b53853bafa7623febb8cfc012c7
Vasil Vanchuk <v.vanchuk@tut.by>
2016-02-04 20:07:18 +0300

    Test model relation

--- tests/unit/chirp/model-test.js
+++ tests/unit/chirp/model-test.js
@@ -1,4 +1,5 @@
 import { moduleForModel, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleForModel('chirp', 'Unit | Model | chirp', {
   // Specify the other units that are required for this test.
@@ -9,4 +10,16 @@ test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('user relationship', function(assert) {
+  // Get the chirp model
+  var Chirp = this.store().modelFor('chirp');
+
+  // Get its 'user'-key
+  var relationship = Ember.get(Chirp, 'relationshipsByName').get('user');
+
+  // Make sure that the relationship works as expected
+  assert.equal(relationship.key, 'user');
+  assert.equal(relationship.kind, 'belongsTo');
 });
